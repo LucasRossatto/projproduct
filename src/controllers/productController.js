993136f9d8f35660bd71070eb7e154bf5ce0696a
LucasRossatto <lucas.sant.anagn3@gmail.com>
2024-03-19 16:42:36 -0300

    1.2 fix package.json

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,9 @@
 const productRepository = require("../services/productRepository");
 
+// Função para obter todos os produtos
 exports.getAllProducts = async (req, res) => {
   try {
+    // Chama a função do repositório para obter todos os produtos
     const products = await productRepository.getAllProducts();
     // Se for bem-sucedida, retorna os produtos com status 200
     res.status(200).json(products);
@@ -20,18 +22,42 @@ exports.getProductById = async (req, res) => {
     if (!product) {
       res.status(404).json({ error: "Produto não encontrado" });
     } else {
+      // Caso contrário, retorna o produto encontrado com status 200
       res.status(200).json(product);
     }
   } catch (err) {
+    // Se houver um erro, retorna um status 500 com a mensagem de erro
     res.status(500).json({ error: err.toString() });
   }
 };
 
-
+// Função para criar um novo produto
 exports.createProduct = async (req, res) => {
   try {
-    
-  } catch (error) {
-    
+    // Chama a função do repositório para criar um novo produto com os dados do corpo da requisição
+    const product = await productRepository.createProduct(req.body);
+    // Se for bem-sucedido, retorna o produto criado com status 201
+    res.status(201).json(product);
+  } catch (err) {
+    // Se houver um erro, retorna um status 500 com uma mensagem indicando o erro
+    res.status(500).json({ error: "Erro ao criar produto" });
   }
-}
+};
+
+// Função para atualizar um produto existente
+exports.updateProduct = async (req, res) => {
+  try {
+    // Chama a função do repositório para atualizar um produto com base no ID fornecido
+    const product = await productRepository.update(req.params.id, req.body);
+    // Se o produto não for encontrado, retorna um status 404 com uma mensagem
+    if (!product) {
+      res.status(404).json({ error: "Produto não encontrado" });
+    } else {
+      // Caso contrário, retorna o produto atualizado com status 200
+      res.status(200).json(product);
+    }
+  } catch (err) {
+    // Se houver um erro, retorna um status 500 com a mensagem de erro
+    res.status(500).json({ error: err.toString() });
+  }
+};
