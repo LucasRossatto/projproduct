e3456a9d45dfb994c0ce82cf51614a1dbf25c5a8
LucasRossatto <lucas.sant.anagn3@gmail.com>
2024-03-19 14:54:34 -0300

    main

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -8,3 +8,25 @@ exports.getAllProducts = async (req, res) => {
     res.status(500).json({ error: err.toString() });
   }
 };
+
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await productRepository.getProductById(req.params.id);
+    if (!product) {
+      res.status(404).json({ error: "Produto não encontrado" });
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.toString() });
+  }
+};
+
+
+exports.createProduct = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
